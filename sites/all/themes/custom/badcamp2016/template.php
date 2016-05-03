1aed275be152650b5404af6963a11bc03a7bbc05
Jayme Johnson <jayme@lastcallmedia.com>
2016-05-03 11:39:39 -0400

    add foundation, create subtheme, jQuery update, some initial styling, font import links, etc.

---
+++ sites/all/themes/custom/badcamp2016/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function badcamp2016_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function badcamp2016_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function badcamp2016_preprocess_node(&$variables) {
+}
