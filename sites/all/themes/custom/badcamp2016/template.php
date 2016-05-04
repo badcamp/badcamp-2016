bab8900311d81a75f7a78e194768002a207932d9
Rob Bayliss <rob@lastcallmedia.com>
2016-05-04 16:45:24 -0400

    Commit the compiled assets

--- sites/all/themes/custom/badcamp2016/template.php
+++ sites/all/themes/custom/badcamp2016/template.php
@@ -10,10 +10,72 @@ function badcamp2016_preprocess_html(&$variables) {
  * Implements template_preprocess_page.
  */
 function badcamp2016_preprocess_page(&$variables) {
+  drupal_set_message('testing');
+}
+
+function badcamp2016_styleguide_alter(&$guide) {
+  $messages = array(
+    'status' => 'success',
+    'warning' => 'warning',
+    'error' => 'alert',
+  );
+  foreach($messages as $type => $class) {
+    $guide[$type . '-message']['content'] = '<div class="callout ' . $class . '">' . styleguide_sentence() . '</div>';
+  }
 }
 
 /**
  * Implements template_preprocess_node.
  */
 function badcamp2016_preprocess_node(&$variables) {
+}
+
+function badcamp2016_preprocess_button(&$variables) {
+  $variables['element']['#attributes']['class'][] = 'button';
+}
+
+function badcamp2016_status_messages($variables) {
+  $display = $variables['display'];
+  $output = '';
+
+  $status_heading = array(
+    'error' => t('Error message'),
+    'status' => t('Status message'),
+    'warning' => t('Warning message'),
+  );
+
+  $status_mapping = array(
+    'error' => 'alert',
+    'status' => 'success',
+    'warning' => 'warning'
+  );
+
+  foreach (drupal_get_messages($display) as $type => $messages) {
+    if (isset($status_mapping[$type])) {
+      $output .= "<div data-alert class=\"callout $status_mapping[$type]\">\n";
+    }
+    else {
+      $output .= "<div data-alert class=\"callout alert\">\n";
+    }
+
+    if (!empty($status_heading[$type])) {
+      $output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+    }
+    if (count($messages) > 1) {
+      $output .= " <ul class=\"no-bullet\">\n";
+      foreach ($messages as $message) {
+        $output .= '  <li>' . $message . "</li>\n";
+      }
+      $output .= " </ul>\n";
+    }
+    else {
+      $output .= $messages[0];
+    }
+
+    $output .= '<button class="close-button"><span aria-hidden="true">&times;</span></button>';
+
+    $output .= "</div>\n";
+  }
+
+  return $output;
 }
