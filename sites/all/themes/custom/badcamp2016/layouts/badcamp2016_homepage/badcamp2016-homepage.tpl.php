be99b2ffbce0ed785c9c7079cc7ad129b66f4fb6
Jayme Johnson <jayme@lastcallmedia.com>
2016-05-04 17:44:46 -0400

    add new page and homepage layouts to their respective features, add a div to each main section on homepage template

--- sites/all/themes/custom/badcamp2016/layouts/badcamp2016_homepage/badcamp2016-homepage.tpl.php
+++ sites/all/themes/custom/badcamp2016/layouts/badcamp2016_homepage/badcamp2016-homepage.tpl.php
@@ -16,79 +16,88 @@
 <?php !empty($css_id) ? print '<div id="' . $css_id . '">' : ''; ?>
 
 <?php if ($content['hero']): ?>
-  <div class="row region-hud">
-    <div class="medium-12 columns">
-      <?php print $content['hero']; ?>
+  <div class="region-hero">
+    <div class="row">
+      <div class="medium-12 columns">
+        <?php print $content['hero']; ?>
+      </div>
     </div>
   </div>
 <?php endif; ?>
-<div class="row region-about">
-  <?php if ($content['about-header']): ?>
-    <div class="medium-12 columns region-about-header">
-      <?php print $content['about-header']; ?>
-    </div>
-  <?php endif; ?>
-  <?php if ($content['about-copy']): ?>
-    <div class="medium-12 columns region-about-copy">
-      <?php print $content['about-copy']; ?>
-    </div>
-  <?php endif; ?>
 
-  <?php if ($content['about-fr-left']): ?>
-  <div class="medium-4 columns region-about-first">
-    <?php print $content['about-fr-left']; ?>
+<div class="region-about">
+  <div class="row">
+    <?php if ($content['about-header']): ?>
+      <div class="medium-12 columns region-about-header">
+        <?php print $content['about-header']; ?>
+      </div>
+    <?php endif; ?>
+    <?php if ($content['about-copy']): ?>
+      <div class="medium-12 columns region-about-copy">
+        <?php print $content['about-copy']; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($content['about-fr-left']): ?>
+      <div class="medium-4 columns region-about-first">
+        <?php print $content['about-fr-left']; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($content['about-fr-middle']): ?>
+      <div class="medium-4 columns region-about-first">
+        <?php print $content['about-fr-middle']; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($content['about-fr-right']): ?>
+      <div class="medium-4 columns region-about-first">
+        <?php print $content['about-fr-right']; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($content['about-sr']): ?>
+      <div class="medium-8 columns region-about-second">
+        <?php print $content['about-sr']; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($content['about-tr-top']): ?>
+      <div class="medium-4 columns region-about-third">
+        <?php print $content['about-tr-top']; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($content['about-tr-bottom']): ?>
+      <div class="medium-4 columns region-about-third">
+        <?php print $content['about-tr-bottom']; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($content['about-button']): ?>
+      <div class="medium-12 columns region-about-button">
+        <?php print $content['about-button']; ?>
+      </div>
+    <?php endif; ?>
   </div>
-  <?php endif; ?>
-  
-  <?php if ($content['about-fr-middle']): ?>
-  <div class="medium-4 columns region-about-first">
-    <?php print $content['about-fr-middle']; ?>
-  </div>
-  <?php endif; ?>
-  
-  <?php if ($content['about-fr-right']): ?>
-    <div class="medium-4 columns region-about-first">
-      <?php print $content['about-fr-right']; ?>
-    </div>
-  <?php endif; ?>
-  
-  <?php if ($content['about-sr']): ?>
-    <div class="medium-8 columns region-about-second">
-      <?php print $content['about-sr']; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($content['about-tr-top']): ?>
-    <div class="columns region-about-third">
-      <?php print $content['about-tr-top']; ?>
-    </div>
-  <?php endif; ?>
-  
-  <?php if ($content['about-tr-bottom']): ?>
-    <div class="columns region-about-third">
-      <?php print $content['about-tr-bottom']; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($content['about-button']): ?>
-    <div class="medium-12 columns region-about-button">
-      <?php print $content['about-button']; ?>
-    </div>
-  <?php endif; ?>
 </div>
 
 <?php if ($content['news']): ?>
-  <div class="row region-news">
-    <div class="medium-8 columns">
-      <?php print $content['news']; ?>
+  <div class="region-news">
+    <div class="row">
+      <div class="medium-8 columns">
+        <?php print $content['news']; ?>
+      </div>
     </div>
   </div>
 <?php endif; ?>
 
 <?php if ($content['sponsors']): ?>
-  <div class="row region-sponsors">
-    <div class="medium-12 columns">
-      <?php print $content['sponsors']; ?>
+  <div class="region-sponsors">
+    <div class="row">
+      <div class="medium-12 columns">
+        <?php print $content['sponsors']; ?>
+      </div>
     </div>
   </div>
 <?php endif; ?>
