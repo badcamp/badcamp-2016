d98aed5090e79cfa4097ede3b4cef9ccc6394c61
Rob Bayliss <rob@lastcallmedia.com>
2016-05-06 18:11:20 -0400

    DrupalCS cleanup in theme

--- sites/all/themes/custom/badcamp2016/template.php
+++ sites/all/themes/custom/badcamp2016/template.php
@@ -1,38 +1,51 @@
 <?php
+/**
+ * @file
+ * Badcamp 2016 theme overrides and preprocess functions.
+ */
 
 /**
- * Implements template_preprocess_html().
+ * Implements hook_preprocess_html().
  */
 function badcamp2016_preprocess_html(&$variables) {
 }
 
 /**
- * Implements template_preprocess_page.
+ * Implements hook_preprocess_page().
  */
 function badcamp2016_preprocess_page(&$variables) {
 }
 
+/**
+ * Implements hook_styleguide_alter().
+ */
 function badcamp2016_styleguide_alter(&$guide) {
   $messages = array(
     'status' => 'success',
     'warning' => 'warning',
     'error' => 'alert',
   );
-  foreach($messages as $type => $class) {
+  foreach ($messages as $type => $class) {
     $guide[$type . '-message']['content'] = '<div class="callout ' . $class . '">' . styleguide_sentence() . '</div>';
   }
 }
 
 /**
- * Implements template_preprocess_node.
+ * Implements hook_preprocess_node().
  */
 function badcamp2016_preprocess_node(&$variables) {
 }
 
+/**
+ * Implements hook_preprocess_button().
+ */
 function badcamp2016_preprocess_button(&$variables) {
   $variables['element']['#attributes']['class'][] = 'button';
 }
 
+/**
+ * Override of theme_status_messages().
+ */
 function badcamp2016_status_messages($variables) {
   $display = $variables['display'];
   $output = '';
@@ -46,15 +59,15 @@ function badcamp2016_status_messages($variables) {
   $status_mapping = array(
     'error' => 'alert',
     'status' => 'success',
-    'warning' => 'warning'
+    'warning' => 'warning',
   );
 
   foreach (drupal_get_messages($display) as $type => $messages) {
     if (isset($status_mapping[$type])) {
-      $output .= "<div data-alert class=\"callout $status_mapping[$type]\">\n";
+      $output .= "<div data-alert class=\"callout messages {$status_mapping[$type]}\">\n";
     }
     else {
-      $output .= "<div data-alert class=\"callout alert\">\n";
+      $output .= "<div data-alert class=\"callout messages alert\">\n";
     }
 
     if (!empty($status_heading[$type])) {
