93c5ca27a8d10117ecbc08b18f6b607b36385b8f
Jayme Johnson <jayme@lastcallmedia.com>
2016-06-08 12:34:21 -0400

    formatting and styling for user reg page

--- sites/all/themes/custom/badcamp2016/dist/js/badcamp2016.js
+++ sites/all/themes/custom/badcamp2016/dist/js/badcamp2016.js
@@ -16,9 +16,8 @@
   // Use document ready because there's no reason to re-build the
   // starfield on ajax.
   $(document).ready(function () {
-    
     // Only show the starfield for the home page
-    if ($('body').hasClass ('page-home')) {
+    if ($('body').hasClass('page-home')) {
       new StarField('layout-wrapper').render(60, 4.6);
     }
   });
---
+++ sites/all/themes/custom/badcamp2016/layouts/badcamp2016_narrow1col/badcamp2016-narrow1col.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Template for Badcamp single column layout.
+ */
+?>
+<div class="region-content">
+  <div class="row">
+    <div class="medium-6 medium-push-3">
+      <?php print $content['content']; ?>
+    </div>
+  </div>
+</div>
