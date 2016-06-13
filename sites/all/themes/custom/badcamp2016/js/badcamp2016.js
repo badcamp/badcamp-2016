91c3c574376b3a47cd68c74fe941b1aaaab7a364
Jayme Johnson <jayme@lastcallmedia.com>
2016-06-13 12:14:41 -0400

    working on reg page 2

--- sites/all/themes/custom/badcamp2016/js/badcamp2016.js
+++ sites/all/themes/custom/badcamp2016/js/badcamp2016.js
@@ -13,12 +13,18 @@
     }
   };
 
-  // Use document ready because there's no reason to re-build the
-  // starfield on ajax.
+
   $(document).ready(function () {
+    // Use document ready because there's no reason to re-build the
+    // starfield on ajax.
     // Only show the starfield for the home page
     if ($('body').hasClass('page-home')) {
       new StarField('layout-wrapper').render(60, 4.6);
+    }
+    //
+    var donationOptions = $('.form-item-stripe-donation-options');
+    if (donationOptions.length === 1) {
+      var elem = new Foundation.Equalizer(donationOptions, {'data-equalize-by-row': true});
     }
   });
 
