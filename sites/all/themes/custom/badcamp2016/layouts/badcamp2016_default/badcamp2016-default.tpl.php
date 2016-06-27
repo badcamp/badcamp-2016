700fe9d02e3506c220734f170416e0f06e412060
Rob Bayliss <rob@lastcallmedia.com>
2016-06-27 16:41:32 -0400

    Adjust margins on default page layout

--- sites/all/themes/custom/badcamp2016/layouts/badcamp2016_default/badcamp2016-default.tpl.php
+++ sites/all/themes/custom/badcamp2016/layouts/badcamp2016_default/badcamp2016-default.tpl.php
@@ -8,19 +8,15 @@
 <?php !empty($css_id) ? print '<div id="' . $css_id . '">' : ''; ?>
 
 
-<div class="row">
-  <div class="row columns">
-    <?php if ($content['title']) : ?>
-      <div class="columns region-title">
-        <?php print $content['title']; ?>
-      </div>
-    <?php endif; ?>
-  </div>
-</div>
+  <?php if ($content['title']) : ?>
+    <div class="row columns region-title">
+      <?php print $content['title']; ?>
+    </div>
+  <?php endif; ?>
 
 
-<div class="region-default-content row">
-  <div class="row columns">
+<div class="row columns">
+  <div class="region-default-content">
     <?php print $content['content']; ?>
   </div>
 </div>
