e23fe31b17b2b789e97c86ed3a34da2a65ec4414
Jon Peck <jpeck@fluxsauce.com>
2016-06-28 18:53:21 -0700

    Cleanup and adding Volunteer page.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -69,6 +69,7 @@ $conf['master_modules']['base'] = array(
   'badcamp_contact_form',
   'badcamp_environment',
   'badcamp_events',
+  'badcamp_feature_page_volunteer',
   'badcamp_homepage',
   'badcamp_mailchimp',
   'badcamp_news_feature',
