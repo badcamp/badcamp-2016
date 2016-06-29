b3b3bdd2fb9f822e85e977575943b5abf041dadd
Jon Peck <jpeck@fluxsauce.com>
2016-06-28 18:17:07 -0700

    Fixing alias.

--- drush/badcamp2016.aliases.drushrc.php
+++ drush/badcamp2016.aliases.drushrc.php
@@ -3,7 +3,7 @@
 // Drupal VM.
 $aliases['local'] = array(
   'uri' => 'badcamp2016.dev',
-  'root' => '/vagrant',
+  'root' => '/var/www',
   'remote-host' => 'badcamp2016.dev',
   'remote-user' => 'vagrant',
   'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key',
