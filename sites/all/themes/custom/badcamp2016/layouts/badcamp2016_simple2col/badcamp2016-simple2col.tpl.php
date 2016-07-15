20f902d01292b4cc6c8530a1af010e608a1d4ee9
Jayme Johnson <jayme@lastcallmedia.com>
2016-07-15 11:11:06 -0400

    create custom add session page refs #17734

---
+++ sites/all/themes/custom/badcamp2016/layouts/badcamp2016_simple2col/badcamp2016-simple2col.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Template for Badcamp two column layout with a 3-9 grid layout.
+ */
+?>
+
+<div class="region-content">
+  <div class="row">
+    <?php if ($content['column_left']) : ?>
+      <div class="medium-6  columns">
+        <?php print $content['column_left']; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($content['column_right']) : ?>
+      <div class="medium-6 columns">
+        <?php print $content['column_right']; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+</div>
