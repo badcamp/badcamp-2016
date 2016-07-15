ef8b0c3e63035bfb4f449c8687a37665cef1bb0e
Rob Bayliss <rob@lastcallmedia.com>
2016-07-15 18:35:28 -0400

    Remove foundation detach, since it does not work and break things

--- sites/all/themes/custom/badcamp2016/dist/js/badcamp2016.js
+++ sites/all/themes/custom/badcamp2016/dist/js/badcamp2016.js
@@ -7,9 +7,6 @@
   Drupal.behaviors.foundation = {
     attach: function (context) {
       $(context).foundation();
-    },
-    detach: function (context) {
-      $(context).foundation('destroy');
     }
   };
 
