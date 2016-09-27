4d97ee7817df525a4d8c5bae28c92393e897de3c
Clayton <csalem@lastcallmedia.com>
2016-09-27 08:54:17 -0400

    Add mailchimp form to footer.

--- sites/all/themes/custom/badcamp2016/layouts/badcamp2016_page/badcamp2016-page.tpl.php
+++ sites/all/themes/custom/badcamp2016/layouts/badcamp2016_page/badcamp2016-page.tpl.php
@@ -61,6 +61,33 @@
             <?php print $content['footer_left']; ?>
           </div>
           <div class="medium-6 columns">
+            <div id="mc_embed_signup" class="form">
+              <form action="//badcamp.us2.list-manage.com/subscribe/post?u=86bbbd957bb1386b180469d43&amp;id=6bd2811fef" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                  <div class="form__field-group">
+                    <div class="mc-field-group form__field">
+                      <label for="mce-EMAIL" class="element-invisible">Email Address</label>
+                      <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="email address *">
+                    </div>
+                    <div class="mc-field-group form__field">
+                      <label for="mce-MMERGE1" class="element-invisible">First Name </label>
+                      <input type="text" value="" name="MMERGE1" class="" id="mce-MMERGE1" placeholder="first name *">
+                    </div>
+                  </div>
+                  <div id="mce-responses" class="clear">
+                    <div class="response" id="mce-error-response" style="display:none"></div>
+                    <div class="response" id="mce-success-response" style="display:none"></div>
+                  </div>
+                  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                  <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                    <input type="text" name="b_86bbbd957bb1386b180469d43_6bd2811fef" tabindex="-1" value="">
+                  </div>
+                  <div class="submit">
+                    <input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" class="button">
+                  </div>
+                </div>
+              </form>
+            </div>
             <?php print $content['footer_right']; ?>
           </div>
         </div>
